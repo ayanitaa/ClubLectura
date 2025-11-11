65d692edfc42b59da414ee3b93350eb94381e470
ayanitaa <ayanasalazar7@gmail.com>
2025-11-10 20:59:11 -0500

    Creación

---
+++ Backend-ClubLectura/src/main/java/com/clublectura/Backend_ClubLectura/BackendClubLecturaApplication.java
@@ -0,0 +1,13 @@
+package com.clublectura.Backend_ClubLectura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendClubLecturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendClubLecturaApplication.class, args);
+	}
+
+}
---
+++ Backend-ClubLectura/src/test/java/com/clublectura/Backend_ClubLectura/BackendClubLecturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clublectura.Backend_ClubLectura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendClubLecturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
